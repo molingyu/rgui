3014e6cf1760f4b2900a54b7ec353e992ad8293a
molingyu <z1522716486@hotmail.com>
2018-04-05 17:23:32 +0800

    change loader style

---
+++ lib/index.rb
@@ -0,0 +1,8 @@
+# encoding:utf-8
+require_relative 'rgss/index'
+require_relative 'event/event_manager'
+require_relative 'action/action_manager'
+require_relative 'action/actions/index'
+require_relative 'collision/collision_manager'
+require_relative 'components/index'
+require_relative 'rgui'
--- lib/rgui.rb
+++ lib/rgui.rb
@@ -1,11 +1,7 @@
 # encoding:utf-8
 
+require_relative 'rgss/index'
 require_relative 'version'
-require_relative 'event/event_manager'
-require_relative 'action/action_manager'
-require_relative 'action/actions/index'
-require_relative 'collision/collision_manager'
-require_relative 'components/index'
 
 module RGUI
 
