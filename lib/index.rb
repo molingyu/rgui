06cc1597a8cbdb1119c9da224aadc86c21d90529
molingyu <z1522716486@hotmail.com>
2018-04-10 22:30:58 +0800

    add label

--- lib/index.rb
+++
@@ -1,9 +0,0 @@
-# encoding:utf-8
-require_relative 'rgss/index'
-require_relative 'event/event_manager'
-require_relative 'action/actions/index'
-require_relative 'action/action_manager'
-require_relative 'collision/index'
-require_relative 'components/index'
-require_relative 'resource'
-require_relative 'rgui'
