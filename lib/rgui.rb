2d5203de5791e64a3a1a18c43e4a5391bfab7b9a
molingyu <z1522716486@hotmail.com>
2018-04-10 22:14:04 +0800

    delete index files

--- lib/rgui.rb
+++ lib/rgui.rb
@@ -1,12 +1,11 @@
 # encoding:utf-8
 
 require_relative 'rgss/index'
+require_relative 'event/event_manager'
+require_relative 'action/actions/index'
+require_relative 'action/action_manager'
+require_relative 'collision/index'
+require_relative 'components/index'
+require_relative 'resource'
 require_relative 'version'
 
-module RGUI
-
-  MOUSE = true
-  KEYBOARD = true
-  CONTROLS = 0
-
-end
