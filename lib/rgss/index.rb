0b4b8b28b8ff688d6439e4e05bc52356f4acfe65
molingyu <z1622716486@hotmail.com>
2023-09-22 16:35:23 +0800

    update: version

--- lib/rgss/index.rb
+++ lib/rgss/index.rb
@@ -1,9 +1,9 @@
 # encoding:utf-8
 
 require_relative 'rgss_base'
-require_relative 'api'
+require_relative 'api' # runtime=rgm
 require_relative 'timer'
 require_relative 'rect'
-require_relative 'input'
+require_relative 'input' # runtime=rgm;rgd
 require_relative 'bitmap'
 require_relative 'color'
